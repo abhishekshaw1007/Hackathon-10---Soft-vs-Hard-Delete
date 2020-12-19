637cfe712c357b66d7277fca33c76c848c3638c1
abhishekshaw1007 <abhishekshaw1007@gmail.com>
2020-12-19 18:50:30 +0530

    Trial 2

--- src/app.js
+++ src/app.js
@@ -11,31 +11,45 @@ app.use(express.json());
 // Get all the students
 app.get("/students", async (req, res) => {
   // write your codes here
-  res.json(Student);
+  res.send(await Student.find({ isDeleted: false }));
 });
 
 // Add student to database
 app.post("/students", async (req, res) => {
   // write your codes here
+  const body = req.body;
+  const newStudent = Student(body);
+  await newStudent.save();
+  res.send(newStudent);
 });
 
 // Get specific student
 app.get("/students/:id", async (req, res) => {
   // write your codes here
-  const id = req.params.id;
-  const studWithId = Student.find({ _id: id });
-  res.json(studWithId);
+  try {
+    const studentWithGivenID = await Student.findOne({
+      _id: req.params.id,
+      isDeleted: false,
+    });
+    if (studentWithGivenID != null && studentWithGivenID != undefined) {
+      res.send(studentWithGivenID);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (er) {
+    res.sendStatus(404);
+  }
 });
 
 // delete specific student
 app.delete("/students/:id", async (req, res) => {
-  // write your codes here
-  const typeOfDelete = req.query;
-  if (typeOfDelete == "soft") {
-    Student.isDeleted = true;
-    res.sendStatus(200);
-  } else if (typeOfDelete == "hard") {
-  } else res.sendStatus(400);
+  // write your codes
+  if (req.query.type.toLowerCase() === "soft") {
+    await Student.updateOne({ _id: req.params.id }, { isDeleted: true });
+  } else if (req.query.type.toLowerCase() === "hard") {
+    await Student.deleteOne({ _id: req.params.id });
+  }
+  res.sendStatus(200);
 });
 
 module.exports = app;
